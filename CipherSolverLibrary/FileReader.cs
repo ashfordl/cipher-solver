9774b1093a6e1d86f748a0704abab9a9fb40ec37
ashfordl <luke.a.ashford@gmail.com>
2015-09-12 20:01:49 +0100

    Read cryptographic data from JSON files, rather than hard coding it.

---
+++ CipherSolverLibrary/FileReader.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace CipherSolver
+{
+    class FileReader
+    {
+        public static Dictionary<char, double> ReadLetterFrequencies()
+        {
+            string file = File.ReadAllText("letter_frequencies.json");
+
+            Dictionary<char, double> freq = JsonConvert.DeserializeObject<Dictionary<char, double>>(file);
+
+            return freq;
+        }
+
+        public static List<string> ReadBigrams()
+        {
+            string file = File.ReadAllText("bigrams.json");
+
+            List<string> bigrams = JsonConvert.DeserializeObject<List<string>>(file);
+
+            return bigrams;
+        }
+    }
+}
