61600d40a61dea85476eb9ae5f25871fe7faa63b
It423 <It423@github.com>
2015-09-14 22:10:25 +0100

    Add class for cipher
    Add a static class for running encryptions and decryptions of columner
    transposition ciphers.

---
+++ CipherSolverLibrary/Ciphers/Transposition.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CipherSolverLibrary.Ciphers
+{
+    public static class Transposition
+    {
+        /// <summary>
+        /// Encrypts a given plaintext using a columner transposition cipher
+        /// </summary>
+        /// <returns>The encrypted message</returns>
+        public static string Encrypt(string plaintext, string key)
+        {
+
+        }
+
+        /// <summary>
+        /// Decrypts a given ciphertext using a columner transposition cipher
+        /// </summary>
+        /// <returns>The decrypted plaintext</returns>
+        public static string Decrypt(string ciphertext, string key)
+        {
+
+        }
+    }
+}
