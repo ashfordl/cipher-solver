060f942b6f6a6f8d57dda7ac406bdf4ee7f6ee1f
It423 <It423@github.com>
2015-09-16 22:24:32 +0100

    Start writing transposition encrypt function
    Write most of the encyrpt function for the transposition cipher, including
    getting key data and sorting the text into columns.

--- CipherSolverLibrary/Ciphers/Transposition.cs
+++ CipherSolverLibrary/Ciphers/Transposition.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CipherSolver.Analysis;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -14,14 +15,71 @@ namespace CipherSolverLibrary.Ciphers
         /// <returns>The encrypted message</returns>
         public static string Encrypt(string plaintext, string key, bool removeRepeats = true)
         {
+            List<int> keyData = GetKeyData(key, removeRepeats);
+            List<string> columns = SplitToColumns(plaintext, keyData.Count);
+            columns = OrderColumns();
 
+            StringBuilder builder = new StringBuilder();
+            foreach (string column in columns)
+            {
+                builder.Append(column);
+            }
+
+            return builder.ToString();
         }
 
         /// <summary>
         /// Decrypts a given ciphertext using a columner transposition cipher
         /// </summary>
-        /// <returns>The decrypted plaintext</returns>
+        /// <returns>The decrypted plaintext</returns>  
         public static string Decrypt(string ciphertext, string key, bool removeRepeats = true)
+        {
+            List<int> keyData = GetKeyData(key, removeRepeats);
+
+            return string.Empty;
+        }
+
+        /// <summary>
+        /// Convernts a key into key data to be used
+        /// </summary>
+        /// <returns>The key data</returns>
+        private static List<int> GetKeyData(string key, bool removeRepeats = true)
+        {
+            if (removeRepeats)
+            {
+                key = Alphabet.RemoveDuplicates(key);
+            }
+
+            return Alphabet.StringToNumbers(key);
+        }
+
+        /// <summary>
+        /// Splits a keytext into columns to be used
+        /// </summary>
+        /// <returns>A list of columns</returns>
+        private static List<string> SplitToColumns(string plaintext, int keyLength)
+        {
+            StringBuilder[] builders = new StringBuilder[keyLength];
+            for (int i = 0; i < keyLength; i++)
+            {
+                builders[i] = new StringBuilder();
+            }
+
+            for (int i = 0; i < plaintext.Length; i++)
+            {
+                builders[i % keyLength].Append(plaintext[i]);
+            }
+
+            List<string> res = builders.Select(b => b.ToString()).ToList();
+
+            return res;
+        }
+
+        /// <summary>
+        /// Orders the columns by the key
+        /// </summary>
+        /// <returns>The columns after being ordered</returns>
+        private static List<string> OrderColumns(List<string> columns, List<int> key)
         {
 
         }
