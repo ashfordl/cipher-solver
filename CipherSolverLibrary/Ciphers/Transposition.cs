ad04a99cb9b87f5307f9d619eb1c9070009c60ae
ashfordl <luke.a.ashford@gmail.com>
2015-09-25 18:54:32 +0100

    Implement decrypt functionality for columnar transposition ciphers

--- CipherSolverLibrary/Ciphers/Transposition.cs
+++ CipherSolverLibrary/Ciphers/Transposition.cs
@@ -1,9 +1,10 @@
-﻿using CipherSolver.Analysis;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CipherSolver;
+using CipherSolver.Analysis;
 
 namespace CipherSolverLibrary.Ciphers
 {
@@ -16,13 +17,14 @@ namespace CipherSolverLibrary.Ciphers
         public static string Encrypt(string plaintext, string key, bool removeRepeats = true)
         {
             List<int> keyData = GetKeyData(key, removeRepeats);
-            List<string> columns = SplitToColumns(plaintext, keyData.Count);
-            columns = OrderColumns();
+            List<string> columns = PlaintextToColumns(plaintext, keyData.Count);
+            var orderedColumns = columns.Select((c, i) => new { Column = c, Index = keyData[i] })
+                             .OrderBy(i => i.Index);
 
             StringBuilder builder = new StringBuilder();
-            foreach (string column in columns)
+            foreach (var col in orderedColumns)
             {
-                builder.Append(column);
+                builder.Append(col.Column);
             }
 
             return builder.ToString();
@@ -35,8 +37,31 @@ namespace CipherSolverLibrary.Ciphers
         public static string Decrypt(string ciphertext, string key, bool removeRepeats = true)
         {
             List<int> keyData = GetKeyData(key, removeRepeats);
+            // Orders keyData alphabetically
+            var orderedKey = keyData.OrderBy(v => v).ToList();
+            // Finds the order to read columns (eg tomato = 421042)
+            var keyPos = keyData.Select((val, ind) => orderedKey.IndexOf(val)).ToList();
 
-            return string.Empty;
+            // Split the ciphertext into columns
+            string[] cols = Transposition.CiphertextToColumns(ciphertext, keyPos);
+
+            StringBuilder builder = new StringBuilder();
+
+            int longest = cols.Select(c => c.Length).Max();
+
+            // Loop through rows then through columns and build the plaintext
+            for (int i = 0; i < longest; i++)
+            {
+                for (int j = 0; j < cols.Length; j++)
+                {
+                    if (i < cols[j].Length)
+                    {
+                        builder.Append(cols[j][i]);
+                    }
+                }
+            }
+
+            return builder.ToString().ToLower();
         }
 
         /// <summary>
@@ -57,7 +82,7 @@ namespace CipherSolverLibrary.Ciphers
         /// Splits a keytext into columns to be used
         /// </summary>
         /// <returns>A list of columns</returns>
-        private static List<string> SplitToColumns(string plaintext, int keyLength)
+        private static List<string> PlaintextToColumns(string plaintext, int keyLength)
         {
             StringBuilder[] builders = new StringBuilder[keyLength];
             for (int i = 0; i < keyLength; i++)
@@ -76,12 +101,50 @@ namespace CipherSolverLibrary.Ciphers
         }
 
         /// <summary>
-        /// Orders the columns by the key
+        /// Splits the ciphertext into columns for decrypting
         /// </summary>
-        /// <returns>The columns after being ordered</returns>
-        private static List<string> OrderColumns(List<string> columns, List<int> key)
+        /// <param name="ciphertext">The ciphertext to split</param>
+        /// <param name="keyLength">The length of the key</param>
+        /// <returns>A list of the columns</returns>
+        private static string[] CiphertextToColumns(string ciphertext, List<int> keyPos)
         {
+            int keyLength = keyPos.Count;
+            int baselen = ciphertext.Length / keyLength;
+            // The number of characters appended onto the end of columns
+            int excess = ciphertext.Length - (baselen * keyLength);
+            // Finds the length corresponding to each column
+            var colLens = keyPos.Select((val, i) => new { Value = val, Length = excess > i ? baselen + 1 : baselen });
 
+            string[] cols = new string[keyLength];
+
+            // Iterate once for each unique key letter
+            for (int i = 0; i < keyLength; i++)
+            {
+                // Finds the columns headered by this key letter
+                var relevantColumns = colLens.Where(len => len.Value == i)
+                                                .Select(c => c.Length);
+                
+                int totalLength = relevantColumns.Sum();
+                int count = relevantColumns.Count();
+
+                // Split the relevant ciphertext into the two columns
+                string[] splitCols = ciphertext.Substring(0, totalLength).SplitByNth(count);
+
+                // Remove the used ciphertext from the remaining ciphertext
+                ciphertext = ciphertext.Substring(totalLength);
+
+                // Add the columns created to the output
+                foreach (string column in splitCols)
+                {
+                    int index = keyPos.IndexOf(i);
+                    cols[index] = column;
+                    keyPos[index] = -1;
+                }
+
+                i += count - 1;
+            }
+
+            return cols;
         }
     }
 }
---
+++ CipherSolverLibrary/TextExtensions.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CipherSolverLibrary
+{
+    /// <summary>
+    /// Contains various extension methods for working with strings and chars
+    /// </summary>
+    static class TextExtensions
+    {
+        /// <summary>
+        /// Splits a string by the nth character and returns an array containing every offset substring
+        /// </summary>
+        /// <param name="s">The string to operate on</param>
+        /// <param name="n">The number of arrays in which to split the string</param>
+        /// <returns>An array of the substrings</returns>
+        public static string[] SplitByNth(this string s, int n)
+        {
+            string[] output = new string[n];
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                output[i % n] += s[i];
+            }
+
+            return output;
+        }
+    }
+}
