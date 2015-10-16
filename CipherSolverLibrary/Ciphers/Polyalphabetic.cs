53ca23031686731ebbcaeb7706689b9ab4a17073
wrightg42 <it42@hotmail.co.uk>
2015-10-16 18:53:44 +0100

    Add polyalphabetic encrypt/decrypt

--- CipherSolverLibrary/Ciphers/Polyalphabetic.cs
+++ CipherSolverLibrary/Ciphers/Polyalphabetic.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CipherSolver.Analysis;
 
 namespace CipherSolver.Ciphers
 {
@@ -14,7 +15,18 @@ namespace CipherSolver.Ciphers
         /// <returns>The encrypted message</returns>
         public static string Encrypt(string plaintext, string key)
         {
-            
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = 0; i < plaintext.Length; i++)
+            {
+                char c = plaintext[i];
+                char k = key[i % key.Length];
+
+                int newValue = Alphabet.IndexOf(c) + Alphabet.IndexOf(k);
+                sb.Append(Alphabet.LetterAt(newValue % 26, true));
+            }
+
+            return sb.ToString();
         }
 
         /// <summary>
@@ -23,7 +35,18 @@ namespace CipherSolver.Ciphers
         /// <returns>The decrypted plaintext</returns>
         public static string Decrypt(string ciphertext, string key)
         {
-            
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = 0; i < ciphertext.Length; i++)
+            {
+                char c = ciphertext[i];
+                char k = key[i % key.Length];
+
+                int newValue = Alphabet.IndexOf(c) - Alphabet.IndexOf(k);
+                sb.Append(Alphabet.LetterAt((newValue + 26) % 26, false));
+            }
+
+            return sb.ToString();
         }
     }
 }
