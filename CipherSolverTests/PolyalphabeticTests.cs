8e61be0e47030c6336c1bcd7ccef15378721a3ce
wrightg42 <it42@hotmail.co.uk>
2015-10-16 18:32:45 +0100

    Write polyalphabetic cipher tests

---
+++ CipherSolverTests/PolyalphabeticTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CipherSolver.Ciphers;
+
+namespace CipherSolverTests
+{
+    /// <summary>
+    /// Summary description for PolyalphabeticTests
+    /// </summary>
+    [TestClass]
+    public class PolyalphabeticTests
+    {
+        [TestMethod]
+        public void Encrypt()
+        {
+            Assert.AreEqual(Polyalphabetic.Encrypt("attackatdawn", "lemon"),
+                "LXFOPVEFRNHR");
+        }
+
+        [TestMethod]
+        public void Decrypt()
+        {
+            Assert.AreEqual(Polyalphabetic.Decrypt("LXFOPVEFRNHR", "lemon"),
+                "attackatdawn");
+        }
+    }
+}
