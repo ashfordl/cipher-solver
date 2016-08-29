d393c8a5e2418dc12d3d6477a95a5cd749a3791d
ashfordl <luke.a.ashford@gmail.com>
2016-08-29 11:47:50 +0100

    Add basic command registering system

---
+++ ConsoleSolver/Commands/ReadCommand.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleSolver.Commands
+{
+    static class ReadCommand
+    {
+
+        public static void Run(List<string> args)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- ConsoleSolver/Program.cs
+++ ConsoleSolver/Program.cs
@@ -3,18 +3,42 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Reflection;
+using ConsoleSolver.Commands;
 
 namespace ConsoleSolver
 {
+    public delegate void Command(List<string> args);
+
     class Program
     {
+        private static Dictionary<string, Command> Commands { get; set; }
+
+        static Program()
+        {
+            RegisterCommands();
+        }
+
+
+        private static void RegisterCommands()
+        {
+            Commands = new Dictionary<string, Command>();
+
+            Commands.Add("read", ReadCommand.Run);
+        }
+
         static void Main(string[] args)
         {
+            // Commands are registered by the static constructor
+            Console.WriteLine("ConsoleSolver");
+
             // Main input loop
             while (true)
             {
-                
+                Console.Write(">>>");
+                string input = Console.ReadLine();
             }
         }
+
     }
 }
