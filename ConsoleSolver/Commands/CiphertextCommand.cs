805559d2f5134fd767f9fbfe68c8327f3372ff5f
ashfordl <luke.a.ashford@gmail.com>
2016-08-29 17:22:25 +0100

    Update cipher command to be able to set the ciphertext

--- ConsoleSolver/Commands/CiphertextCommand.cs
+++ ConsoleSolver/Commands/CiphertextCommand.cs
@@ -14,8 +14,33 @@ namespace ConsoleSolver.Commands
         /// <param name="args">Ignored</param>
         public static void Run(List<string> args)
         {
-            Console.WriteLine(CipherData.CipherText);
-            Console.WriteLine("________________\nLength {0} chars", CipherData.CipherText.Length);
+            if (args.Count == 1)
+            {
+                Console.WriteLine(CipherData.CipherText);
+                Console.WriteLine("________________\nLength {0} chars", CipherData.CipherText.Length);
+                return;
+            }
+
+            string arg2 = args[1].ToUpper();
+
+            if (arg2 == "SET")
+            {
+                // Reset ciphertext
+                CipherData.CipherText = "";
+                // Remove the first two arguments (cipher & set) so they are not included
+                args.RemoveRange(0, 2);
+                // Append each chunk to the ciphertext, restoring spaces
+                args.ForEach(s => CipherData.CipherText += (" " + s));
+                // Remove leading space from first loop iteration
+                CipherData.CipherText = CipherData.CipherText.Remove(0, 1);
+            }
+            else
+            {
+                Console.WriteLine("If no arguments are provided, the current value of the working "
+                                 +"ciphertext will be printed. Alternatively, this value may be updated.");
+                Console.WriteLine("Format: cipher set <new value>\t<new value> may contain any character.");
+                return;
+            }
         }
     }
 }
